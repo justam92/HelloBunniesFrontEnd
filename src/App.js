acfb4f57203ed7c044b7426dbfbc7d71097bb7b4
justam92 <31111330+justam92@users.noreply.github.com>
2019-10-08 21:45:54 +0200

    Changed navbar position on the page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar'
+import Logo from './components/Logo'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+    <div className="App container-fluid">
+      <div className="row">
+          <Logo />
+          <Navbar />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className="nav-wrapper red darken-3">
-            <div className="container">
-                <a className="brand-logo">Hello Bunnies!</a>
-                <ul className="right">
-                    <li><a href="/">Home</a> </li>
-                    <li><a href="/nourishmentCategory">Nourishment Category</a> </li>
-                    <li><a href="/poison">Poison</a> </li>
-                    <li><a href="/care">Care</a> </li>
-                    <li><a href="/diseaseCategory">Disease Category</a> </li>
-                    <li><a href="/firstBunny">First Bunny</a> </li>
-                    <li><a href="/firstAid">First Aid</a> </li>
-                    <li><a href="/animalRight">Animal Right</a> </li>
-                </ul>
+        <nav className="navbar navbar-inverse navbar-fixed-top navbar-fixed-right">
+            <div id="wrapper" class="toggled">
+                <div id="sidebar-wrapper">
+                    <ul className="nav navbar-nav">
+                        <li><a href="/">Home</a> </li>
+                        <li><a href="/nourishmentCategory">Nourishment Category</a> </li>
+                        <li><a href="/poison">Poison</a> </li>
+                        <li><a href="/care">Care</a> </li>
+                        <li><a href="/diseaseCategory">Disease Category</a> </li>
+                        <li><a href="/firstBunny">First Bunny</a> </li>
+                        <li><a href="/firstAid">First Aid</a> </li>
+                        <li><a href="/animalRight">Animal Right</a> </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
