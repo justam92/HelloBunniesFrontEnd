0d8a1c5fe7670325dffa243bcc21d77a340f1f65
justam92 <31111330+justam92@users.noreply.github.com>
2019-10-08 21:42:54 +0200

    Added Logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+        <div className="col">
+            <a href="/" className="navbar-brand">Hello Bunnies!</a>
+        </div>
+    )
+}
+
+export default Logo
