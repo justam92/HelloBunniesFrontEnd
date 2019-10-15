8a99ce762ba2589e6446879172c04b991cd1b848
justam92 <31111330+justam92@users.noreply.github.com>
2019-10-15 19:11:22 +0200

    Modified navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar'
 import Logo from './components/Logo'
 import { BrowserRouter, Route } from 'react-router-dom'
@@ -13,116 +13,61 @@ import OtherNourishment from './components/OtherNourishment'
 import Care from './components/Care'
 import Disease from './components/Disease'
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <div className="App container-fluid">
-        <div className="row">
-        <Navbar />
-          <Logo />
-          <div className="w-100"></div>
-          <Route exact path='/' component={Home} />
-          <Route path='/animalRight' component={AnimalRight} />
-          <Route path='/poison' component={Poison} />
-          <Route path='/firstBunny' component={FirstBunny} />
-          <Route path='/firstAid' component={FirstAid} />
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <div className="row">
+            <Navbar />
+            <Logo />
+            <Route exact path='/' component={Home} />
+            <Route path='/animalRight' component={AnimalRight} />
+            <Route path='/poison' component={Poison} />
+            <Route path='/firstBunny' component={FirstBunny} />
+            <Route path='/firstAid' component={FirstAid} />
 
-          <Route path='/vegetables' component={Nourishment} />
-          <Route path='/fruits' component={Nourishment} />
-          <Route path='/herbs' component={Nourishment} />
+            <Route path='/vegetables' component={Nourishment} />
+            <Route path='/fruits' component={Nourishment} />
+            <Route path='/herbs' component={Nourishment} />
 
-          <Route path='/seedAndCereal' component={SeedAndCereal} />
-          <Route path='/otherNourishment' component={OtherNourishment} />
+            <Route path='/seedAndCereal' component={SeedAndCereal} />
+            <Route path='/otherNourishment' component={OtherNourishment} />
 
-          <Route path='/bunnySpeech' component={Care} />
-          <Route path='/bunnyCage' component={Care} />
-          <Route path='/bunnyLittleBoxAndLearnUseIt' component={Care} />
-          <Route path='/homeSecurity' component={Care} />
-          <Route path='/bunnyToys' component={Care} />
-          <Route path='/companionshipForBunny' component={Care} />
-          <Route path='/tripWithBunny' component={Care} />
-          <Route path='/walkWithBunny' component={Care} />
-          <Route path='/pregnancyAndNewborns' component={Care} />
-          <Route path='/raisingBunny' component={Care} />
-          <Route path='/bunnyPhysiology' component={Care} />
-          <Route path='/tamingBunny' component={Care} />
-          <Route path='/bunnyCare' component={Care} />
+            <Route path='/bunnySpeech' component={Care} />
+            <Route path='/bunnyCage' component={Care} />
+            <Route path='/bunnyLittleBoxAndLearnUseIt' component={Care} />
+            <Route path='/homeSecurity' component={Care} />
+            <Route path='/bunnyToys' component={Care} />
+            <Route path='/companionshipForBunny' component={Care} />
+            <Route path='/tripWithBunny' component={Care} />
+            <Route path='/walkWithBunny' component={Care} />
+            <Route path='/pregnancyAndNewborns' component={Care} />
+            <Route path='/raisingBunny' component={Care} />
+            <Route path='/bunnyPhysiology' component={Care} />
+            <Route path='/tamingBunny' component={Care} />
+            <Route path='/bunnyCare' component={Care} />
 
-          <Route path='/osteoporosis' component={Disease} />
-          <Route path='/brokenBone' component={Disease} />
-          <Route path='/spinalInjury' component={Disease} />
+            <Route path='/boneDiseases' component={Disease} />
+            <Route path='/eyeDiseases' component={Disease} />
+            <Route path='/earDiseases' component={Disease} />
+            <Route path='/urinaryTractDiseases' component={Disease} />
+            <Route path='/respiratorySystemDiseases' component={Disease} />
+            <Route path='/digestiveSystemDiseases' component={Disease} />
+            <Route path='/reproductiveSystemDiseases' component={Disease} />
+            <Route path='/externalParasiticDiseases' component={Disease} />
+            <Route path='/internalParasiticDiseases' component={Disease} />
+            <Route path='/skinDiseases' component={Disease} />
+            <Route path='/diseasesThatRequireVaccination' component={Disease} />
+            <Route path='/dentalDiseases' component={Disease} />
+            <Route path='/otherDiseases' component={Disease} />
 
-          <Route path='/glaucoma' component={Disease} />
-          <Route path='/cataract' component={Disease} />
-          <Route path='/blindBunny' component={Disease} />
-          <Route path='/eyeRemoval' component={Disease} />
-          <Route path='/eyeDamage' component={Disease} />
-          <Route path='/tearDuctClogged' component={Disease} />
-          <Route path='/conjunctivitis' component={Disease} />
+            <Route path='/firstAidKit' component={Disease} />
 
-          <Route path='/otitis' component={Disease} />
-          <Route path='/earMites' component={Disease} />
-          <Route path='/deafBunny' component={Disease} />
-
-          <Route path='/sandAndStones' component={Disease} />
-          <Route path='/nephritis' component={Disease} />
-          <Route path='/cystitis' component={Disease} />
-
-          <Route path='/rhinitis' component={Disease} />
-          <Route path='/cold' component={Disease} />
-          <Route path='/choking' component={Disease} />
-          <Route path='/pneumonia' component={Disease} />
-
-          <Route path='/bezoars' component={Disease} />
-          <Route path='/gastroenteritis' component={Disease} />
-          <Route path='/diarrhea' component={Disease} />
-          <Route path='/flatulence' component={Disease} />
-          <Route path='/constipation' component={Disease} />
-          <Route path='/bacterialEnteritis' component={Disease} />
-          <Route path='/stomachOverflow' component={Disease} />
-
-          <Route path='/vaginitis' component={Disease} />
-          <Route path='/uterineInflammation' component={Disease} />
-          <Route path='/mastitis' component={Disease} />
-          <Route path='/genitalCancer' component={Disease} />
-
-          <Route path='/cheyletiellaParasitivorax' component={Disease} />
-          <Route path='/ticks' component={Disease} />
-          <Route path='/fleas' component={Disease} />
-          <Route path='/trombiculaAutumnalis' component={Disease} />
-          <Route path='/sarcoptesScabiei' component={Disease} />
-          <Route path='/mallophaga' component={Disease} />
-          <Route path='/anoplura' component={Disease} />
-          <Route path='/larvaeOfFlies' component={Disease} />
-          <Route path='/demodexCanis' component={Disease} />
-          <Route path='/encephalitozoonCuniculi' component={Disease} />
-          <Route path='/coccidiosis' component={Disease} />
-          <Route path='/nematodes' component={Disease} />
-
-          <Route path='/tinea' component={Disease} />
-          <Route path='/plantarDermatitis' component={Disease} />
-          <Route path='/cheilitis' component={Disease} />
-
-          <Route path='/myxomatosis' component={Disease} />
-          <Route path='/bunnyPlague' component={Disease} />
-          <Route path='/pasteurellosis' component={Disease} />
-
-          <Route path='/toothCrownHypertrophy' component={Disease} />
-          <Route path='/toothRootHypertrophy' component={Disease} />
-          <Route path='/abscesses' component={Disease} />
-          <Route path='/toothFractures' component={Disease} />
-
-          <Route path='/diabetes' component={Disease} />
-          <Route path='/epilepsy' component={Disease} />
-          <Route path='/sunstroke' component={Disease} />
-          <Route path='/feeding' component={Disease} />
-          <Route path='/mycotoxins' component={Disease} />
-
-          <Route path='/firstAidKit' component={Disease} />
-
+          </div>
         </div>
-      </div>
-    </BrowserRouter>
-  );
+      </BrowserRouter>
+    );
+  }
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div className="container menu">
+        <div className="container-fluid menu">
             <nav className="navbar navbar-collapse navbar-fixed-top">
                 <div id="wrapper" className="toggled">
                     <div id="sidebar-wrapper">
@@ -48,168 +48,24 @@ const Navbar = () => {
                                 </div>
                             </li>
 
-                            {/* <li>Disease Category</li> */}
-
                             <li className="btn-group dropdown">
-
                                 <ul className="dropdown-menu">
-                                    <li className="btn-group dropleft">
-                                        <ul className="dropdown-menu">
-                                            <Link to='/osteoporosis'> <li className="dropdown-item">Osteoporoza</li> </Link>
-                                            <Link to='/brokenBone'> <li className="dropdown-item">Złamanie</li> </Link>
-                                            <Link to='/spinalInjury'> <li className="dropdown-item">Uraz kręgosłupa</li> </Link>
-                                        </ul>
-                                        <div className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Choroby kości
-                                    </div>
-                                    </li>
 
-                                    <li className="btn-group dropleft">
-                                        <ul className="dropdown-menu">
-                                            <Link to='/glaucoma'> <li className="dropdown-item">Jaskra</li> </Link>
-                                            <Link to='/cataract'> <li className="dropdown-item">Zaćma (katarakta)</li> </Link>
-                                            <Link to='/blindBunny'> <li className="dropdown-item">Królik niewidomy</li> </Link>
-                                            <Link to='/eyeRemoval'> <li className="dropdown-item">Usunięcie oka</li> </Link>
-                                            <Link to='/eyeDamage'> <li className="dropdown-item">Uszkodzenie oka</li> </Link>
-                                            <Link to='/tearDuctClogged'> <li className="dropdown-item">Zatkany kanalik łzowy</li> </Link>
-                                            <Link to='/conjunctivitis'> <li className="dropdown-item">Zapalenie spojówki</li> </Link>
-                                        </ul>
-                                        <div className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Choroby oczu
-                                        </div>
-                                    </li>
-
-                                    <li className="btn-group dropleft">
-                                        <ul className="dropdown-menu">
-                                            <Link to='/otitis'> <li className="dropdown-item">Zapalenie uszu</li> </Link>
-                                            <Link to='/earMites'> <li className="dropdown-item">Świerzb uszny</li> </Link>
-                                            <Link to='/deafBunny'> <li className="dropdown-item">Królik niesłyszący</li> </Link>
-                                        </ul>
-                                        <div className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Choroby uszu
-                                        </div>
-                                    </li>
-
-                                    <li className="btn-group dropleft">
-                                        <ul className="dropdown-menu">
-                                            <Link to='/sandAndStones'> <li className="dropdown-item">Piasek i kamienie</li> </Link>
-                                            <Link to='/nephritis'> <li className="dropdown-item">Zapalenie nerek</li> </Link>
-                                            <Link to='/cystitis'> <li className="dropdown-item"> Zapalenie pęcherza </li> </Link>
-                                        </ul>
-                                        <div className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Choroby układu moczowego
-                                        </div>
-                                    </li>
-
-                                    <li className="btn-group dropleft">
-                                        <ul className="dropdown-menu">
-                                            <Link to='/rhinitis'> <li className="dropdown-item">Katar</li> </Link>
-                                            <Link to='/cold'> <li className="dropdown-item">Przeziębienie</li> </Link>
-                                            <Link to='/choking'> <li className="dropdown-item">Zakrztuszenie/zadławienie</li> </Link>
-                                            <Link to='/pneumonia'> <li className="dropdown-item">Zapalenie płuc</li> </Link>
-                                        </ul>
-                                        <div className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Choroby układu oddechowego
-                                        </div>
-                                    </li>
-
-                                    <li className="btn-group dropleft">
-                                        <ul className="dropdown-menu">
-                                            <Link to='/bezoars'> <li className="dropdown-item">Bezoary (Kule włosowe)</li> </Link>
-                                            <Link to='/gastroenteritis'> <li className="dropdown-item">Nieżyt żołądka i jelit</li> </Link>
-                                            <Link to='/diarrhea'> <li className="dropdown-item">Rozwolnienie</li> </Link>
-                                            <Link to='/flatulence'> <li className="dropdown-item">Wzdęcia</li> </Link>
-                                            <Link to='/constipation'> <li className="dropdown-item">Zaparcia</li> </Link>
-                                            <Link to='/bacterialEnteritis'> <li className="dropdown-item">Bakteryjne zapalenie jelit</li> </Link>
-                                            <Link to='/stomachOverflow'> <li className="dropdown-item">Przepełnienie żołądka</li> </Link>
-                                        </ul>
-                                        <div className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Choroby układu pokarmowego
-                                        </div>
-                                    </li>
-
-                                    <li className="btn-group dropleft">
-                                        <ul className="dropdown-menu">
-                                            <Link to='/vaginitis'> <li className="dropdown-item">Zapalenie pochwy</li> </Link>
-                                            <Link to='/uterineInflammation'> <li className="dropdown-item">Zapalenie macicy</li> </Link>
-                                            <Link to='/mastitis'> <li className="dropdown-item">Zapalenie gruczołów mlecznych</li> </Link>
-                                            <Link to='/genitalCancer'> <li className="dropdown-item">Rak narządów płciowych</li> </Link>
-                                        </ul>
-                                        <div className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Choroby układu rozrodczego
-                                        </div>
-                                    </li>
-
-                                    <li className="btn-group dropleft">
-                                        <ul className="dropdown-menu">
-                                            <p>Zewnętrzne</p>
-                                            <Link to='/cheyletiellaParasitivorax'> <li className="dropdown-item">Chejletieloza</li> </Link>
-                                            <Link to='/ticks'> <li className="dropdown-item">Kleszcze</li> </Link>
-                                            <Link to='/fleas'> <li className="dropdown-item">Pchły</li> </Link>
-                                            <Link to='/trombiculaAutumnalis'> <li className="dropdown-item">Swędzik jesienny</li> </Link>
-                                            <Link to='/sarcoptesScabiei'> <li className="dropdown-item">Świerzb drażniący</li> </Link>
-                                            <Link to='/mallophaga'> <li className="dropdown-item">Wszoły</li> </Link>
-                                            <Link to='/anoplura'> <li className="dropdown-item">Wszy</li> </Link>
-                                            <Link to='/larvaeOfFlies'> <li className="dropdown-item">Larwy much</li> </Link>
-                                            <Link to='/demodexCanis'> <li className="dropdown-item">Nużyca</li> </Link>
-                                            <p>Wewnętrzne</p>
-                                            <Link to='/encephalitozoonCuniculi'> <li className="dropdown-item">Encephalitozoon cuniculi</li> </Link>
-                                            <Link to='/coccidiosis'> <li className="dropdown-item">Kokcydioza</li> </Link>
-                                            <Link to='/nematodes'> <li className="dropdown-item">Nicienie</li> </Link>
-                                        </ul>
-                                        <div className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Choroby pasożytnicze
-                                        </div>
-                                    </li>
-
-                                    <li className="btn-group dropleft">
-                                        <ul className="dropdown-menu">
-                                            <Link to='/tinea'> <li className="dropdown-item">Grzybica</li> </Link>
-                                            <Link to='/plantarDermatitis'> <li className="dropdown-item">Zapalenie skóry podeszwy</li> </Link>
-                                            <Link to='/cheilitis'> <li className="dropdown-item">Zapalenie warg</li> </Link>
-                                        </ul>
-                                        <div className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Choroby skóry
-                                        </div>
-                                    </li>
-
-                                    <li className="btn-group dropleft">
-                                        <ul className="dropdown-menu">
-                                            <Link to='/myxomatosis'> <li className="dropdown-item">Myksomatoza</li> </Link>
-                                            <Link to='/bunnyPlague'> <li className="dropdown-item">Pomór króliczy (VHD)</li> </Link>
-                                            <Link to='/pasteurellosis'> <li className="dropdown-item">Pastereloza</li> </Link>
-                                        </ul>
-                                        <div className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Choroby wymagające szczepień
-                                        </div>
-                                    </li>
-
-                                    <li className="btn-group dropleft">
-                                        <ul className="dropdown-menu">
-                                            <Link to='/toothCrownHypertrophy'> <li className="dropdown-item">Przerost korony</li> </Link>
-                                            <Link to='/toothRootHypertrophy'> <li className="dropdown-item">Przerost korzenia</li> </Link>
-                                            <Link to='/abscesses'> <li className="dropdown-item">Ropnie</li> </Link>
-                                            <Link to='/toothFractures'> <li className="dropdown-item">Złamania zębów</li> </Link>
-                                        </ul>
-                                        <div className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Choroby zębów
-                                        </div>
-                                    </li>
-
-                                    <li className="btn-group dropleft">
-                                        <ul className="dropdown-menu">
-                                            <Link to='/diabetes'> <li className="dropdown-item">Cukrzyca</li> </Link>
-                                            <Link to='/epilepsy'> <li className="dropdown-item">Padaczka (epilepsja)</li> </Link>
-                                            <Link to='/sunstroke'> <li className="dropdown-item">Udar słoneczny</li> </Link>
-                                            <Link to='/feeding'> <li className="dropdown-item">Dokarmianie</li> </Link>
-                                            <Link to='/mycotoxins'> <li className="dropdown-item">Mykotoksyny</li> </Link>
-                                        </ul>
-                                        <div className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Inne choroby
-                                        </div>
-                                    </li>
-
+                                    <Link to='/boneDiseases'> <li className="dropdown-item"> Choroby kości </li> </Link>
+                                    <Link to='/eyeDiseases'> <li className="dropdown-item"> Choroby oczu </li> </Link>
+                                    <Link to='/earDiseases'> <li className="dropdown-item"> Choroby uszu </li> </Link>
+                                    <Link to='/urinaryTractDiseases'> <li className="dropdown-item"> Choroby układu moczowego </li> </Link>
+                                    <Link to='/respiratorySystemDiseases'> <li className="dropdown-item"> Choroby układu oddechowego </li> </Link>
+                                    <Link to='/digestiveSystemDiseases'> <li className="dropdown-item"> Choroby układu pokarmowego </li> </Link>
+                                    <Link to='/reproductiveSystemDiseases'> <li className="dropdown-item"> Choroby układu rozrodczego </li> </Link>
+                                    <Link to='/externalParasiticDiseases'> <li className="dropdown-item"> Choroby pasożytnicze zewnętrzne</li> </Link>
+                                    <Link to='/internalParasiticDiseases'> <li className="dropdown-item"> Choroby pasożytnicze wewnętrzne</li> </Link>
+                                    <Link to='/skinDiseases'> <li className="dropdown-item"> Choroby skóry </li> </Link>
+                                    <Link to='/diseasesThatRequireVaccination'> <li className="dropdown-item"> Choroby wymagające szczepień </li> </Link>
+                                    <Link to='/dentalDiseases'> <li className="dropdown-item"> Choroby zębów </li> </Link>
+                                    <Link to='/otherDiseases'> <li className="dropdown-item"> Inne choroby </li> </Link>
                                     <Link to='/firstAidKit'> <li className="dropdown-item">Apteczka</li> </Link>
+
                                 </ul>
                                 <div className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     Choroby
