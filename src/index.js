62898d5d993f221cae4116a844b2f35b182007f9
justam92 <31111330+justam92@users.noreply.github.com>
2019-10-15 19:15:49 +0200

    Changed folder in imported index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import bootstrap from 'bootstrap';
 
 ReactDOM.render(<App />, document.getElementById('root'));
