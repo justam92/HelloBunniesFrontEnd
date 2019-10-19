ae79ea225632ba819ed1a781da143bed6fd66266
justam92 <31111330+justam92@users.noreply.github.com>
2019-10-19 23:43:47 +0200

    Improve import bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import bootstrap from 'bootstrap';
+import 'bootstrap';
 
 ReactDOM.render(<App />, document.getElementById('root'));
