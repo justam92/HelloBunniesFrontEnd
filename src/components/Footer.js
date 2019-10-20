d5f32cbd6a80d35ef65e6d6648f9ecadec32395d
justam92 <31111330+justam92@users.noreply.github.com>
2019-10-20 16:32:24 +0200

    Added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="container-fluid fixed-bottom footerContent">
+
+                <p>Nie jestem autorem treści. Treść pobrana ze strony <a href="http://www.miniaturkabeztajemnic.com/">Miniaturka Bez Tajemnic</a></p>
+                <p>Korzystam z treści w celu dostarczenia danych testowych.</p>
+
+            </div>
+        )
+    }
+}
